afabaca60d7ebc1a77392189ea24a93fa0041018
n-peugnet <n.peugnet@free.fr>
2020-01-25 04:12:22 +0100

    add License

--- jfmp/app.py
+++ jfmp/app.py
@@ -1,7 +1,19 @@
-# MusicPlayer, https://github.com/albertz/music-player
-# Copyright (c) 2012, Albert Zeyer, www.az2000.de
-# All rights reserved.
-# This code is under the 2-clause BSD license, see License.txt in the root directory of this project.
+# Copyright (C) 2020  Nicolas Peugnet
+#
+# This file is part of jfmp.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import sys
 import os
