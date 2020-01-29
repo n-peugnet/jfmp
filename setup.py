2f9af7a633c307fc4f1e9da15e16c77f3fb9aca9
n-peugnet <n.peugnet@free.fr>
2020-01-29 22:23:16 +0100

    build: use pbr for packaging

--- setup.py
+++ setup.py
@@ -1,28 +1,8 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
-    name="jfmp",
-    version="0.1.0b1",
-    author="Nicolas Peugnet",
-    author_email="n.peugnet@free.fr",
-    description="A minimalist cross-platform gapless music player for Jellyfin",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/n-peugnet/jfmp",
-    packages=['jfmp'],
-    entry_points={
-        "gui_scripts": [
-            "jfmp = jfmp.app:main",
-        ]
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-    ],
+    setup_requires=['pbr>=1.9'],
+    pbr=True,
     python_requires='>=3.6',
     install_requires=[
         "jellyfin-apiclient-python>=1.2,<2",
